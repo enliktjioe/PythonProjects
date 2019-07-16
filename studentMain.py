7348519e1790e297d330d7461fc1a8e786f875ee
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-16 11:33:40 +0700

    Update studentMain.py
    Replace spy

--- studentMain.py
+++ studentMain.py
@@ -1,17 +1,29 @@
-# We defined:
+# Define a procedure, replace_spy,
+# that takes as its input a list of
+# three numbers, and modifies the
+# value of the third element in the
+# input list to be one more than its
+# previous value.
 
-stooges = ['Moe','Larry','Curly']
+spy = [0,0,7]
 
-# but in some Stooges films, Curly was
-# replaced by Shemp.
+def replace_spy(listInput):
+    listInput[2] += 1
+    return listInput
 
-# Write one line of code that changes
-# the value of stooges to be:
 
-# ['Moe','Larry','Shemp']
 
-# but does not create a new List
-# object.
 
-stooges[2] = 'Shemp'
-print(stooges)
+
+
+
+
+
+# In the test below, the first line calls your 
+# procedure which will change spy, and the 
+# second checks you have changed it.
+# Uncomment the top two lines below.
+
+replace_spy(spy)
+print(spy)
+#>>> [0,0,8]
