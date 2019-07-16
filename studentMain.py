a6fc52a8f4238974a1e84a6f4d87c0c507448287
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-16 13:06:24 +0700

    Update studentMain.py
    Loops on List

--- studentMain.py
+++ studentMain.py
@@ -1,7 +1,8 @@
-p =[1,2]
-q = [3,4]
-p = p + q
-print(len(p))
-# answer = 3
-print(p)
-# [1,2,[3,4]]
+def print_all_elements(p):
+    i = 0
+    while i < len(p):
+        print(p[i])
+        i = i + 1
+
+listItem = [5,3,1,6]
+print_all_elements(listItem)
