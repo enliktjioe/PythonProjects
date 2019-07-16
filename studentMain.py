c1ad7fdbee4aba080569b03c17fa817b53e9b3f4
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-16 23:19:23 +0700

    Update studentMain.py
    daysBetweenDates problem with or without leap year

--- studentMain.py
+++ studentMain.py
@@ -1,24 +1,23 @@
-# Define a procedure, find_element,
-# using index that takes as its
-# inputs a list and a value of any
-# type, and returns the index of
-# the first element in the input
-# list that matches the value.
+# Given your birthday and the current date, calculate your age 
+# in days. Compensate for leap days. Assume that the birthday 
+# and current date are correct dates (and no time travel). 
+# Simply put, if you were born 1 Jan 2012 and todays date is 
+# 2 Jan 2012 you are 1 day old.
 
-# If there is no matching element,
-# return -1.
+# IMPORTANT: You don't need to solve the problem yet! 
+# Just brainstorm ways you might approach it!
 
-def find_element(xlist, y):
-    if y in xlist:
-        return xlist.index(y)
-    else:
-        return -1
+daysOfMonths = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+def isLeapYear(year):
+    ##
+    # Your code here. Return True or False
+    # Pseudo code for this algorithm is found at
+    # http://en.wikipedia.org/wiki/Leap_year#Algorithm
+    ##
 
-
-
-print(find_element([1,2,3],3))
-#>>> 2
-
-print(find_element(['alpha','beta'],'gamma'))
-#>>> -1
+def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+    ##
+    # Your code here.
+    ##
+    return days
