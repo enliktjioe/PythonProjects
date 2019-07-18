65a2857d92287644b0ff6936a0ccd101884e068b
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-18 18:35:51 +0700

    Movie Website Creation
    Concept of OOP

--- studentMain.py
+++ studentMain.py
@@ -1,5 +1,10 @@
 import time
 import webbrowser
 
-time.sleep(5) # waiting for 5 second before running the next code
-webbrowser.open("http://www.facebook.com")
+i = 0
+
+print("this program started on " + time.ctime())
+while i<3: # add a loop to this code so it prompts the user to take 3 breaks
+    time.sleep(5) # waiting for 5 second before running the next code
+    webbrowser.open("http://www.facebook.com")
+    i+=1
