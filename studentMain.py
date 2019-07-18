e514f775d98bade9fc6c157e9e0172b40552165f
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-18 18:01:11 +0700

    Putting it all together

--- studentMain.py
+++ studentMain.py
@@ -1,21 +1,33 @@
-# Here's another chance to practice your for loop skills. Write code for the 
-# function word_in_pos (meaning word in parts_of_speech), which takes in a string 
-# word and a list parts_of_speech as inputs. If there is a word in parts_of_speech
-# that is a substring of the variable word, then return that word in parts_of_speech, 
-# else return None.
+# Write code for the function play_game, which takes in as inputs parts_of_speech
+# (a list of acceptable replacement words) and ml_string (a string that 
+# can contain replacement words that are found in parts_of_speech). Your play_game
+# function should return the joined list replaced, which will have the same structure
+# as ml_string, only that replacement words are swapped out with "corgi", since this
+# program cannot replace those words with user input. 
 
+parts_of_speech  = ["PLACE", "PERSON", "PLURALNOUN", "NOUN"]
+
+test_string = """This is PLACE, no NOUN named PERSON, We have so many PLURALNOUN around here."""
 
 def word_in_pos(word, parts_of_speech):
     for pos in parts_of_speech:
         if pos in word:
             return pos
     return None
-
-
-test_cases = ["NOUN", "FALSE", "<<@PERSON><", "PLURALNOUN"]
-parts_of_speech = ["PERSON", "PLURALNOUN", "NOUN"]
-
-print(word_in_pos(test_cases[0], parts_of_speech))
-print(word_in_pos(test_cases[1], parts_of_speech))
-print(word_in_pos(test_cases[2], parts_of_speech))
-print(word_in_pos(test_cases[3], parts_of_speech))
+        
+def play_game(ml_string, parts_of_speech):    
+    replaced = []
+    # your code here
+    ml_string = ml_string.split()
+    for word in ml_string:
+        replacement = word_in_pos(word, parts_of_speech)
+        if replacement != None:
+            word = word.replace(replacement, "Corg,")
+            replaced.append(word)
+        else:
+            replaced.append(word)
+    
+    replaced = " ".join(replaced)
+    return replaced
+    
+print(play_game(test_string, parts_of_speech))  
