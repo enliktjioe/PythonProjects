51ba16424a0540f6df9982962436e433010d3435
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-07-22 12:30:02 +0700

    Learn Dictionary in Python
    Population of some cities

--- studentMain.py
+++ studentMain.py
@@ -1,10 +1,19 @@
-import time
-import webbrowser
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions.
+population = {"Shanghai":17.8,
+              "Istanbul":13.3,
+              "Karachi":13.0,
+              "Mumbai":12.5}
 
-i = 0
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
 
-print("this program started on " + time.ctime())
-while i<3: # add a loop to this code so it prompts the user to take 3 breaks
-    time.sleep(5) # waiting for 5 second before running the next code
-    webbrowser.open("http://www.facebook.com")
-    i+=1
+print(population["Karachi"])
