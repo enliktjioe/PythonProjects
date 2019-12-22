c96199e5ba3a522886e7f4575db4c11854296b26
Enlik <enlikt@taltech.ee>
2019-12-22 13:56:17 +0200

    Updated script to download
    change https to http

--- Positium/test_task_02042019/HelloGIS.py
+++ Positium/test_task_02042019/HelloGIS.py
@@ -3,5 +3,7 @@ import wget
 print('Beginning file download with wget module')
 
 url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-wget.download(url, '/Users/enlik/Downloads/cat4.jpg')
+url2 = 'http://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson'
+# wget.download(url, '/Users/enlik/Downloads/cat4.jpg')
+wget.download(url2, '/Users/enlik/Downloads/countries.geojson')
 
