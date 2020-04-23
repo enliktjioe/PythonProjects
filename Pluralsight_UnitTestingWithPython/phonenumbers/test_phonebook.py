37a9b4444cb588960a797d2ae77473b2034e44de
Enlik - <enlik@ut.ee>
2020-04-24 02:16:14 +0300

    add new project from Pluralsight course

---
+++ Pluralsight_UnitTestingWithPython/phonenumbers/test_phonebook.py
@@ -0,0 +1,14 @@
+import unittest
+
+from phonebook import PhoneBook
+
+
+class PhoneBookTest(unittest.TestCase):
+    
+    def test_lookup_by_name(self):
+        phonebook = PhoneBook()
+        phonebook.add("Bob", "12345")
+        number = phonebook.lookup("Bob")
+        self.assertEqual("12345", number)
+
+# Run in terminal using command "python -m unittest"
